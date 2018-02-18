e61066555a16aee58da10aa57be689ef923eda19
jkang8 <johnkang.h@gmail.com>
2018-02-18 00:48:12 -0800

    Setup sass, form page model

---
+++ contact/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from contact.models import FormPage
+
+
+# Create your views here.
+def contact(request):
+    page = FormPage.objects.get(slug='contact-me')
+    return render(request, 'contact/form_page.html', {
+        'page': page
+        # 'search_query': search_query,
+        # 'search_results': search_results,
+    })
