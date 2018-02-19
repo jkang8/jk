84fa4ad3d11e6dd9470b0269562aae7d87df9b9e
jkang8 <johnkang.h@gmail.com>
2018-02-18 17:52:11 -0800

    Monochrome

--- home/models.py
+++ home/models.py
@@ -5,11 +5,11 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('body', classname="full"),
     ]
