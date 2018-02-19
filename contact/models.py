3cc19161a6cef61b5acf2844332bec3214f4e3e5
jkang8 <johnkang.h@gmail.com>
2018-02-19 01:47:12 -0800

    Adjust textarea col for responsiveness

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,10 @@
-from django.db import models
+from django.forms import widgets
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailadmin.edit_handlers import (FieldPanel, FieldRowPanel, 
         InlinePanel, MultiFieldPanel)
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
+
 
 class FormField(AbstractFormField):
     page = ParentalKey('FormPage', related_name='form_fields')
@@ -25,4 +26,13 @@ class FormPage(AbstractEmailForm):
             FieldPanel('subject'),
         ], "Email"),
     ]
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        # iterate through the fields in the generated form
+        for name, field in form.fields.items():
+            # if the field is a TextArea - adjust the columns 
+            if isinstance(field.widget, widgets.Textarea):
+                field.widget.attrs.update({'cols': '5'})
+        return form
 
