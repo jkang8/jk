de175798fb2ed92172b1962b5723971ea08fcc30
jkang8 <johnkang.h@gmail.com>
2018-02-18 18:42:16 -0800

    Update ALLOWED_HOSTS

--- jk/settings/production.py
+++ jk/settings/production.py
@@ -6,6 +6,11 @@ import django_heroku
 django_heroku.settings(locals())
 DEBUG = False
 
+ALLOWED_HOSTS = (
+    'morning-sea-64598.herokuapp.com',
+    'johnkang.co',
+)
+
 try:
     from .local import *
 except ImportError:
