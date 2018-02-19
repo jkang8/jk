e823aa21fb8aa8a42cdd0537fb464cdbbd2bb35a
jkang8 <johnkang.h@gmail.com>
2018-02-18 22:10:51 -0800

    Re-enable contact url

--- contact/views.py
+++ contact/views.py
@@ -4,7 +4,7 @@ from contact.models import FormPage
 
 # Create your views here.
 def contact(request):
-    page = FormPage.objects.get(slug='contact-me')
+    page = FormPage.objects.get(slug='contact')
     return render(request, 'contact/form_page.html', {
         'page': page
         # 'search_query': search_query,
