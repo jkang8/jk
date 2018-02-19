c976df517f56219257fef8da087a86d596c2303d
jkang8 <johnkang.h@gmail.com>
2018-02-18 19:39:50 -0800

    Revert manage

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if not os.environ["DJANGO_SETTINGS_MODULE"]:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jk.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jk.settings.dev")
 
     from django.core.management import execute_from_command_line
 
